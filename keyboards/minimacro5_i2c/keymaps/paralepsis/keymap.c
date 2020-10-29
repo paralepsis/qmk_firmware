0ffa03a9513504fa7fc633256723782e80e35d42
Rob Ross <rbross@pobox.com>
2020-10-28 21:49:58 -0500

    more fun!

--- keyboards/minimacro5_i2c/keymaps/paralepsis/keymap.c
+++ keyboards/minimacro5_i2c/keymaps/paralepsis/keymap.c
@@ -2,6 +2,7 @@
 
 enum layers {
      _MAIN,
+     _ALT,
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
@@ -20,10 +21,23 @@ void encoder_update_user(uint8_t index, bool clockwise) {
       tap_code16(LGUI(KC_MINUS));
     }
   } else if (index == 2) { /* Third encoder*/
-    if (clockwise) {
-      tap_code(KC_WH_D);
-    } else {
-      tap_code(KC_WH_U);
+    switch(biton32(layer_state)) {
+      case _MAIN:
+        if (clockwise) {
+          tap_code(KC_PGDOWN);
+        } else {
+          tap_code(KC_PGUP);
+        }
+        break;
+      case _ALT:
+        if (clockwise) {
+          tap_code(KC_WH_D);
+        } else {
+          tap_code(KC_WH_U);
+        }
+        break;
+      default:
+        break;
     }
   }
 }
@@ -31,6 +45,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 //
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { //buttion closest to usb is first
   [_MAIN] = LAYOUT_ortho_1x5(
-     KC_MUTE, LGUI(KC_0), KC_3, KC_PGUP, KC_PGDN
+     KC_MUTE, LGUI(KC_0), TO(_ALT), KC_PGUP, KC_PGDN
+  ),
+  [_ALT] = LAYOUT_ortho_1x5(
+     KC_MUTE, LGUI(KC_0), TO(_MAIN), KC_PGUP, KC_PGDN
   )
 };

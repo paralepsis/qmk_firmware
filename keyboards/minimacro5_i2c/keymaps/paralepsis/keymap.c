830e4aa50e7fbc10a32e890c4a9471e08af42351
Rob Ross <rbross@pobox.com>
2020-11-16 16:41:13 -0600

    further cleanup, in progress.

--- keyboards/minimacro5_i2c/keymaps/paralepsis/keymap.c
+++ keyboards/minimacro5_i2c/keymaps/paralepsis/keymap.c
@@ -1,11 +1,11 @@
 #include QMK_KEYBOARD_H
 
-static const char encFnS0[] PROGMEM = "Vol Up/Down/Mute\n";
-static const char encFnS1[] PROGMEM = "Zoom In/Out/Reset\n";
-static const char encFnS2[] PROGMEM = "Page Up/Down\n";
-static const char encFnS3[] PROGMEM = "Scroll Up/Down\n";
-static const char encFnS4[] PROGMEM = "Scroll Left/Right\n";
-
+/* encFn - describes a particular encoder functionality:
+ * - string    - string desc. of the functionality to display on OLED
+ * - clockwise - 16-bit tap code for clockwise (can be modded)
+ * - countercw - 16-bit tap code for counter clockwise (can be modded)
+ * - press     - 16-bit tap code for press (can be modded)
+ */
 struct encFn {
    const char     *string;
    const uint16_t  clockwise;
@@ -13,6 +13,13 @@ struct encFn {
    const uint16_t  press;
 };
 
+static const char encFnS0[] PROGMEM = "Vol Up/Down/Mute\n";
+static const char encFnS1[] PROGMEM = "Zoom In/Out/Reset\n";
+static const char encFnS2[] PROGMEM = "Page Up/Down\n";
+static const char encFnS3[] PROGMEM = "Scroll Up/Down\n";
+static const char encFnS4[] PROGMEM = "Scroll Left/Right\n";
+
+/* myFn - array of functionality options for encoders */
 static struct encFn myFn[] = {
    { encFnS0, KC_VOLU, KC_VOLD, KC_MUTE }, /* 0 */
    { encFnS1, LGUI(KC_EQUAL), LGUI(KC_MINUS), LGUI(KC_0) }, /* 1 */
@@ -21,25 +28,36 @@ static struct encFn myFn[] = {
    { encFnS4, KC_WH_R, KC_WH_L, KC_HOME }, /* 4 */
 };
 
+/* array holding current encoder functionality assignments */
+static int curEncFn[] = { 0, 1, 2 };
+
+/* layers:
+ * - _MAIN - normal operation
+ * - _PROG - mode in which functionality assignment may be modified
+ */
 enum layers {
      _MAIN,
      _PROG,
 };
 
-/* array holding current encoder function assignments */
-static int curEncFn[] = { 0, 1, 2 };
-
 void encoder_update_user(uint8_t index, bool clockwise) {
   if (index > 2) return;
 
-  if (clockwise) {
-    tap_code16(myFn[curEncFn[index]].clockwise);
-  } else {
-    tap_code16(myFn[curEncFn[index]].countercw);
+  switch (get_highest_layer(layer_state)) {
+    case _MAIN:
+      if (clockwise) {
+        tap_code16(myFn[curEncFn[index]].clockwise);
+      } else {
+        tap_code16(myFn[curEncFn[index]].countercw);
+      }
+      break;
+    case _PROG:
+      break;
+    default:
+      break;
   }
 }
 
-//
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { //buttion closest to usb is first
   [_MAIN] = LAYOUT_ortho_1x5(
      KC_MUTE, LGUI(KC_0), TO(_PROG), KC_PGUP, KC_PGDN
